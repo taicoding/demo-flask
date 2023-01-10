fe3834090d97b03956f47d893825ccc610dc7759
taicoding <tai.delgadillo@gmail.com>
2023-01-09 21:11:06 -0400

    doxeando ips :v

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from flask import Flask, request
+from flask import Flask, request, make_response, redirect
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello, Hago un curso hare!"
+def index():
+    user_ip = request.remote_addr
+    response = make_response(redirect("/hello"))
+    response.set_cookie("user_ip", user_ip)
+    return response
+
+
+@app.route("/hello")
+def doxeando_ips():
+    user_ip = request.cookies.get("user_ip")
+    return "Hola :D the hemos doxeado y esta es tu IP: {}".format(user_ip)
 
 
 if __name__ == "__main__":

9a4fc102b0bd40e0df209d485021aac5460ab2a4
taicoding <tai.delgadillo@gmail.com>
2023-01-10 20:46:59 -0400

    agregando un form

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-from flask import Flask, request, make_response, redirect, render_template, session
+from flask import (
+    Flask,
+    request,
+    make_response,
+    redirect,
+    render_template,
+    session,
+    url_for,
+)
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
@@ -12,6 +23,12 @@ app.config["SECRET_KEY"] = "SUPER SECRETO"
 
 
 todos = ["Jugar con el Michi", "Comprar comida del michi", "Amar al michi"]
+
+
+class LoginForm(FlaskForm):
+    username = StringField("Usuario", validators=[DataRequired()])
+    password = PasswordField("Contraseña", validators=[DataRequired()])
+    submit = SubmitField("Enviar")
 
 
 @app.errorhandler(404)
@@ -32,12 +49,23 @@ def index():
     return response
 
 
-@app.route("/hello")
+@app.route("/hello", methods=["GET", "POST"])
 def doxeando_ips():
     user_ip = session.get("user_ip")
-    context = {"user_ip": user_ip, "todos": todos}
+    login_form = LoginForm()
+    username = session.get("username")
+    context = {
+        "user_ip": user_ip,
+        "todos": todos,
+        "login_form": login_form,
+        "username": username,
+    }
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session["username"] = username
+        return redirect(url_for("index"))
     return render_template("hello.html", **context)
 
 
 if __name__ == "__main__":
-    app.run(port=8080)
+    app.run(port=8080, debug=True)
